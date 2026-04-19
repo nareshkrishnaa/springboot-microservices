dbe68b92149f9502a2e43fff053b1860b60d02f0
nareshkrishnaa <nareshkrishnaaa@gmail.com>
2026-04-20 00:09:17 +0530

    Simplified the query in address repository

--- service-communication-feignclient/address-service/src/main/java/dev/learning/addressapp/repository/AddressRepo.java
+++ service-communication-feignclient/address-service/src/main/java/dev/learning/addressapp/repository/AddressRepo.java
@@ -11,9 +11,12 @@ import java.util.Optional;
 @Repository
 public interface AddressRepo extends JpaRepository<Address, Integer> {
 
+
     @Query(
             nativeQuery = true,
             value
-                    = "SELECT ea.id, ea.city, ea.state FROM msdemo.address ea join msdemo.employee e on e.id = ea.employee_id where ea.employee_id=:employeeId")
+                    = "SELECT id, city, state \n" +
+                    "FROM address \n" +
+                    "WHERE employee_id = :employeeId")
     Optional<Address> findAddressByEmployeeId(@Param("employeeId") int employeeId);
 }
